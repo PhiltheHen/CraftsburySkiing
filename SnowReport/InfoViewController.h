1f294c8e4dc09d4a4e94dda21b5c0ec302b32d91
Phil Henson <philip.d.henson@gmail.com>
2013-06-07 16:56:50 -0400

    Added more images, updated weather forecast view controller, and added trail status view controller.

---
+++ SnowReport/InfoViewController.h
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.h
+//  SnowReport
+//
+//  Created by Philip Henson on 6/4/13.
+//  Copyright (c) 2013 Philip Henson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextView *infoText1;
+@property (strong, nonatomic) IBOutlet UITextView *infoText2;
+@property (strong, nonatomic) IBOutlet UITextView *infoText3;
+
+@end
